5072d6c7223ca7fcc595fcc9cdb8efd0637ea455
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2019-03-21 02:13:08 +0200

    Güncelleme

--- dist/formalize.js
+++ dist/formalize.js
@@ -1,32 +1,96 @@
-function hello(compiler) {
-    console.log("Hello from " + compiler);
-}
-hello("TypeScript");
-// class Formalize {
-//     data: any;
-//     constructor() {
-//         this.data = null;
-//     }
-//     setData(data: any):boolean {
-//     }
-//     getData(data: any):any {
-//     }
-//     isEmpty(data: any):boolean {
-//     }
-//     isNull(data: any):boolean {
-//     }
-//     isUndefined(data: any):boolean {
-//     }
-//     isLetter(data: any):boolean {
-//     }
-//     isNumber(data: any):boolean {
-//     }
-//     isLetterOrNumber(data: any):boolean {
-//     }
-//     isMail(data: any):boolean {
-//     }
-//     isPhone(data: any):boolean {
-//     }
-//     isIP(data: any): boolean {
-//     }
-// }
+var Formalize = /** @class */ (function () {
+    function Formalize() {
+        this.data = null;
+    }
+    Formalize.prototype.setData = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+    };
+    Formalize.prototype.getData = function () {
+        return (this.data);
+    };
+    Formalize.prototype.isEmpty = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return (typeof this.data == 'undefined' || this.data.match(/^ *$/) !== null);
+    };
+    Formalize.prototype.isNull = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return ((this.data === null) ? true : false);
+    };
+    Formalize.prototype.isUndefined = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return ((this.data === 'undefined') ? true : false);
+    };
+    Formalize.prototype.isLetter = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return (this.data.match(/^[A-Za-z]+$/) ? true : false);
+    };
+    Formalize.prototype.isNumber = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return (this.data.match(/^[-+]?[0-9]+$/) ? true : false);
+    };
+    Formalize.prototype.isLetterOrNumber = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return (this.data.match(/^[0-9a-zA-Z]+$/) ? true : false);
+    };
+    Formalize.prototype.isMail = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return (this.data.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) ? true : false);
+    };
+    Formalize.prototype.isPhone = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return (this.data.match(/^\d{10}$/) ? true : false);
+    };
+    Formalize.prototype.isIP = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return ((/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/).test(this.data) ? true : false);
+    };
+    Formalize.prototype.isCreditcard = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        // Eklenecek.
+        return (false);
+    };
+    Formalize.prototype.isURL = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return ((/^(http|https|ftp?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/).test(this.data) ? true : false);
+    };
+    Formalize.prototype.isHTML = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return ((/(<([^>]+)>)/i).test(this.data) ? true : false);
+    };
+    Formalize.prototype.isImage = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return (this.data.match(/\.(jpeg|jpg|gif|png)$/) ? true : false);
+    };
+    Formalize.prototype.isColor = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return ((/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i).test(this.data) ? true : false);
+    };
+    // 00:00 - 23:59
+    Formalize.prototype.isTime = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return ((/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/).test(this.data) ? true : false);
+    };
+    // dd/mm/yyyy veya dd.mm.yyyy
+    Formalize.prototype.isDate = function (data) {
+        if (data === void 0) { data = null; }
+        this.data = data || this.data;
+        return (this.data.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/) ? true : false);
+    };
+    return Formalize;
+}());
--- src/formalize.ts
+++ src/formalize.ts
@@ -5,71 +5,124 @@ class Formalize {
         this.data = null;
     }
 
-    setData(data: any):boolean {
-
+    setData(data: any = null): void {
+        this.data = data || this.data;
     }
 
-    getData(data: any):any {
-
+    getData(): any {
+        return (this.data);
     }
 
-    isEmpty(data: any):boolean {
-
+    isEmpty(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            typeof this.data == 'undefined' || this.data.match(/^ *$/) !== null
+        );
     }
 
-    isNull(data: any):boolean {
-
+    isNull(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            (this.data === null) ? true : false
+        );
     }
 
-    isUndefined(data: any):boolean {
-
+    isUndefined(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            (this.data === 'undefined') ? true : false
+        );
     }
 
-    isLetter(data: any):boolean {
-
+    isLetter(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            this.data.match(/^[A-Za-z]+$/) ? true : false
+        );
     }
 
-    isNumber(data: any):boolean {
-
+    isNumber(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            this.data.match(/^[-+]?[0-9]+$/) ? true : false
+        );
     }
 
-    isLetterOrNumber(data: any):boolean {
-
+    isLetterOrNumber(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            this.data.match(/^[0-9a-zA-Z]+$/) ? true : false
+        );
     }
 
-    isMail(data: any):boolean {
-
+    isMail(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            this.data.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) ? true : false
+        );
     }
 
-    isPhone(data: any):boolean {
-
+    isPhone(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            this.data.match(/^\d{10}$/) ? true : false
+        );
     }
 
-    isIP(data: any): boolean {
-        
+    isIP(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/).test(this.data) ? true : false
+        );
     }
 
-    isCreditcard(data: any): boolean {
-
+    isCreditcard(data: any = null): boolean {
+        this.data = data || this.data;
+        // Eklenecek.
+        return (false);
     }
 
-    isURL(data: any): boolean {
-
+    isURL(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            (/^(http|https|ftp?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/).test(this.data) ? true : false
+        );
     }
 
-    isHTML(data: any): boolean {
-
+    isHTML(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            (/(<([^>]+)>)/i).test(this.data) ? true : false
+        );
     }
 
-    isImage(data: any): boolean {
-        
+    isImage(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            this.data.match(/\.(jpeg|jpg|gif|png)$/) ? true : false
+        );
     }
 
-    isTime(data: any): boolean {
-
+    isColor(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i).test(this.data) ? true : false
+        );
     }
 
-    isDate(data: any): boolean {
+    // 00:00 - 23:59
+    isTime(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            (/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/).test(this.data) ? true : false
+        );
+    }
 
+    // dd/mm/yyyy veya dd.mm.yyyy
+    isDate(data: any = null): boolean {
+        this.data = data || this.data;
+        return (
+            this.data.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/) ? true : false
+        );
     }
 }
